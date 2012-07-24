8571c66dfe7ed1e3b929dd49778b03f086f25d44
alberto turinetti <sataniccrow@gmail.com>
2012-07-24 17:11:56 +0200

    removing testing file

--- gen/com/g3ck0/yaamp/R.java
+++ gen/com/g3ck0/yaamp/R.java
@@ -22,16 +22,16 @@ public final class R {
         public static final int yaamp=0x7f020008;
     }
     public static final class id {
-        public static final int NextSongButtonTopPanel=0x7f060002;
-        public static final int buttonStopServiceBottomPanel=0x7f060009;
-        public static final int closeApplication=0x7f060005;
-        public static final int lastSongButtonTopPanel=0x7f060000;
-        public static final int listViewSongsList=0x7f060003;
-        public static final int notifyTextView=0x7f060006;
-        public static final int relativeLayoutTopPanel=0x7f060008;
-        public static final int song=0x7f060007;
-        public static final int stopButtonTopPanel=0x7f060001;
-        public static final int yaampIcon=0x7f060004;
+        public static final int NextSongButtonTopPanel=0x7f050002;
+        public static final int buttonStopServiceBottomPanel=0x7f050009;
+        public static final int closeApplication=0x7f050005;
+        public static final int lastSongButtonTopPanel=0x7f050000;
+        public static final int listViewSongsList=0x7f050003;
+        public static final int notifyTextView=0x7f050006;
+        public static final int relativeLayoutTopPanel=0x7f050008;
+        public static final int song=0x7f050007;
+        public static final int stopButtonTopPanel=0x7f050001;
+        public static final int yaampIcon=0x7f050004;
     }
     public static final class layout {
         public static final int bottom_panel=0x7f030000;
@@ -41,15 +41,12 @@ public final class R {
         public static final int song=0x7f030004;
         public static final int top_panel=0x7f030005;
     }
-    public static final class raw {
-        public static final int catgroove=0x7f040000;
-    }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int app_service_name=0x7f050002;
-        public static final int hello=0x7f050000;
-        public static final int margin_left_right=0x7f050004;
-        public static final int margin_top_down=0x7f050005;
-        public static final int service_name_identifier=0x7f050003;
+        public static final int app_name=0x7f040001;
+        public static final int app_service_name=0x7f040002;
+        public static final int hello=0x7f040000;
+        public static final int margin_left_right=0x7f040004;
+        public static final int margin_top_down=0x7f040005;
+        public static final int service_name_identifier=0x7f040003;
     }
 }
