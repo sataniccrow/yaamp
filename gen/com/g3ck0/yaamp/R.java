9a9dff614c09233046a35e1099fca56465ef26fc
alberto turinetti <sataniccrow@gmail.com>
2012-07-24 12:44:09 +0200

    new grapx: modified layouts, added icons, txt removed

--- gen/com/g3ck0/yaamp/R.java
+++ gen/com/g3ck0/yaamp/R.java
@@ -11,20 +11,24 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ghost=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int leftarrow=0x7f020002;
-        public static final int rightarrow=0x7f020003;
+        public static final int close_button=0x7f020000;
+        public static final int ghost=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int leftarrow=0x7f020003;
+        public static final int leftarrow_thin=0x7f020004;
+        public static final int pause_button=0x7f020005;
+        public static final int rightarrow=0x7f020006;
+        public static final int rightarrow_thin=0x7f020007;
     }
     public static final class id {
-        public static final int NextSongButtonTopPanel=0x7f060007;
-        public static final int buttonBottomPanel=0x7f060000;
-        public static final int buttonStopServiceBottomPanel=0x7f060001;
-        public static final int lastSongButtonTopPanel=0x7f060006;
-        public static final int listViewSongsList=0x7f060002;
-        public static final int notifyTextView=0x7f060003;
-        public static final int relativeLayoutTopPanel=0x7f060005;
-        public static final int song=0x7f060004;
+        public static final int NextSongButtonTopPanel=0x7f060002;
+        public static final int buttonStopServiceBottomPanel=0x7f060007;
+        public static final int lastSongButtonTopPanel=0x7f060000;
+        public static final int listViewSongsList=0x7f060003;
+        public static final int notifyTextView=0x7f060004;
+        public static final int relativeLayoutTopPanel=0x7f060006;
+        public static final int song=0x7f060005;
+        public static final int stopButtonTopPanel=0x7f060001;
     }
     public static final class layout {
         public static final int bottom_panel=0x7f030000;
